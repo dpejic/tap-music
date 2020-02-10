6f7d7cd50703796ae9f292c2d9f86e6e44cae83a
Damjan Pejić <31569910+dpejic@users.noreply.github.com>
2020-02-10 20:55:41 +0100

    Update

---
+++ scripts/script.js
@@ -0,0 +1,12 @@
+window.addEventListener("load", () => {
+    const sounds = document.querySelectorAll(".sound");
+    const pads = document.querySelectorAll(".pads div");
+
+    pads.forEach((pad, index) => {
+        pad.addEventListener("click", () => {
+            console.log("clicked");
+            sounds[index].currentTime = 0;
+            sounds[index].play();
+        });
+    });
+});
